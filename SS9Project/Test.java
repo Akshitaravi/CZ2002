7d96a2a26383645449d710e7fe7fbedf3e5a6fa5
MelvinLeeYJ <67396076+MelvinLeeYJ@users.noreply.github.com>
2020-10-29 12:19:24 +0800

    Update Test.java

--- SS9Project/Test.java
+++ SS9Project/Test.java
@@ -33,6 +33,23 @@ public class Test {
 	}
 
 	public static void main(String[] Argz) {
+		// Get Data
+		ArrayList<Course> listofCourse = new ArrayList<Course>();
+
+		try {
+			FileInputStream fis = new FileInputStream("data/testCourse.dat");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			listofCourse = (ArrayList) ois.readObject();
+			ois.close();
+			fis.close();
+
+			for (int i = 0; i < listofCourse.size(); i++)
+				System.out.println(listofCourse.get(i));
+		} catch (Exception e) {
+			System.out.println("Exception Occured >> " + e.getMessage());
+		}
+
+		// Insert Data (Course)
 		ArrayList<Index> totalindex = new ArrayList<Index>();
 
 		Schedule nSchedule = new Schedule(19201, 4, 2, 5, 3, convertTime("14:30"), convertTime("09:30"),
@@ -42,17 +59,19 @@ public class Test {
 		totalindex.add(index);
 
 		Course nCourse = new Course("OBJECT ORIENTED DESIGN & PROGRAMMING", "CZ2002", totalindex);
+		listofCourse.add(nCourse);
 
 		try {
 			FileOutputStream fos = new FileOutputStream("data/testCourse.dat");
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(nCourse);
+			oos.writeObject(listofCourse);
 			oos.close();
 			fos.close();
 		} catch (Exception e) {
 			System.out.println("Exception Occured >> " + e.getMessage());
 		}
 
+		// Insert Data ( Admin)
 		ArrayList<Admin> totaladmin = new ArrayList<Admin>();
 		Admin admin1 = new Admin("Tan Kheng Leong", "KHENGLEONG", "P@ssw0rd", "S1234567A", newDate("1970-01-01"),
 				87654321, true, "SG", "50", "Nanyang Avenue", "Singapore", "639798");
