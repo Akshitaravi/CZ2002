0c1b4d681bcb91f31b44c28ec68ce13e70aaf6f7
pmeghana07 <71749547+pmeghana07@users.noreply.github.com>
2020-11-14 17:51:52 +0800

    Add files via upload

---
+++ SS9Project/WaitList.java
@@ -0,0 +1,14 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.*;
+
+/*
+ * Each course represent a subject for a degree programme. Each course can have multiple indexes.
+ *
+ * Done by AKSHITA RAVISANKAR, HOO KAH JUN, JEWELLE LIM FONG YEE, LEE YU JIE MELVIN, PUVVADA MEGHANA
+ */
+
+public class WaitList{
+	
+}
