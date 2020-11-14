e79b93f1c58355d83f4d6b28128074c312b26581
pmeghana07 <71749547+pmeghana07@users.noreply.github.com>
2020-11-14 18:02:48 +0800

    Update WaitList.java

--- SS9Project/WaitList.java
+++ SS9Project/WaitList.java
@@ -3,12 +3,57 @@ package entities;
 import java.io.Serializable;
 import java.util.*;
 
+import entities.Index;
+
 /*
- * Each course represent a subject for a degree programme. Each course can have multiple indexes.
+ * Represents the waitlist an Index has.
  *
  * Done by AKSHITA RAVISANKAR, HOO KAH JUN, JEWELLE LIM FONG YEE, LEE YU JIE MELVIN, PUVVADA MEGHANA
  */
 
 public class WaitList{
-	
+	// index number this waitlist is for
+	private String IndexID;
+	//number of students in this waitlist
+	private String numberOfStudentWaitList;
+	// list of students in this waitlist: with their matric numbers
+	private String[] studentWaitList;
+	//index this waitlist belongs to
+	private Index Index;
+	//create a new waitlist
+	public WaitList() {
+	}
+	//get index number this waitlist is for
+	public String getIndexID() {
+		return IndexID;
+	}
+	//change index number this waitlist belongs to
+	public void setIndexID(String IndexID) {
+		this.IndexID = IndexID;
+	}
+	//get index number this waitlist belongs to
+	public Index getIndex() {
+		return Index;
+	}
+	//change the index this waitlist belongs to
+	public void setIndex(Index index) {
+		Index = index;
+	}
+	//gets number of students in this waitlist
+	public String getNumberOfStudentWaitList() {
+		return numberOfStudentWaitList;
+	}
+	//changes number of students in this waitlist
+	public void setNumberOfStudentWaitList(String numberOfStudentWaitList) {
+		this.numberOfStudentWaitList = numberOfStudentWaitList;
+	}
+	//gets list of students in this waitlist: with their matric numbers
+	public String[] getStudentWaitList() {
+		return studentWaitList;
+	}
+	//changes list of students in this waitlist: with their matric numbers
+	public void setStudentWaitList(String[] studentWaitList) {
+		this.studentWaitList = studentWaitList;
+	}
+
 }
