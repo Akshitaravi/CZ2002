b90ca921cbb4e0aca2f10568d1f396da0edb3eae
pmeghana07 <71749547+pmeghana07@users.noreply.github.com>
2020-11-16 17:17:32 +0800

    Update WaitList.java

--- SS9Project/WaitList.java
+++ SS9Project/WaitList.java
@@ -1,6 +1,9 @@
-package SS9Project;
+package entities;
 
-//import entities.Index;
+import java.io.Serializable;
+import java.util.*;
+
+import entities.Index;
 
 /*
  * Represents the waitlist an Index has.
@@ -8,58 +11,57 @@ package SS9Project;
  * Done by AKSHITA RAVISANKAR, HOO KAH JUN, JEWELLE LIM FONG YEE, LEE YU JIE MELVIN, PUVVADA MEGHANA
  */
 
-public class WaitList {
+public class WaitList{
 	// index number this waitlist is for
-	private String IndexID;
-	// number of students in this waitlist
+	//private String IndexID;
+	private int IndexID;
+	//number of students in this waitlist
 	private String numberOfStudentWaitList;
 	// list of students in this waitlist: with their matric numbers
 	private String[] studentWaitList;
-	// index this waitlist belongs to
+	//index this waitlist belongs to
 	private Index Index;
-
-	// create a new waitlist
+	
+	public static ArrayList<WaitList> listofwaitlistStudents;
+	public static Object waitListStudents;
+	
+	//create a new waitlist
 	public WaitList() {
 	}
-
-	// get index number this waitlist is for
-	public String getIndexID() {
+	//get index number this waitlist is for
+	public int getIndexID() {
 		return IndexID;
 	}
-
-	// change index number this waitlist belongs to
-	public void setIndexID(String IndexID) {
+	//change index number this waitlist belongs to
+	public void setIndexID(int IndexID) {
 		this.IndexID = IndexID;
 	}
-
-	// get index number this waitlist belongs to
+	//get index number this waitlist belongs to
 	public Index getIndex() {
 		return Index;
 	}
-
-	// change the index this waitlist belongs to
+	//change the index this waitlist belongs to
 	public void setIndex(Index index) {
 		Index = index;
 	}
-
-	// gets number of students in this waitlist
+	//gets number of students in this waitlist
 	public String getNumberOfStudentWaitList() {
 		return numberOfStudentWaitList;
 	}
-
-	// changes number of students in this waitlist
+	//changes number of students in this waitlist
 	public void setNumberOfStudentWaitList(String numberOfStudentWaitList) {
 		this.numberOfStudentWaitList = numberOfStudentWaitList;
 	}
-
-	// gets list of students in this waitlist: with their matric numbers
-	public String[] getStudentWaitList() {
+	//gets list of students in this waitlist: with their matric numbers
+	public  String[] getStudentWaitList() {
 		return studentWaitList;
 	}
-
-	// changes list of students in this waitlist: with their matric numbers
+	//changes list of students in this waitlist: with their matric numbers
 	public void setStudentWaitList(String[] studentWaitList) {
 		this.studentWaitList = studentWaitList;
 	}
+	
+	
+
 
 }
