4405cc19461e483d1943143314abc143b848eb82
MelvinLeeYJ <67396076+MelvinLeeYJ@users.noreply.github.com>
2020-11-18 14:28:59 +0800

    Update STARS.java

--- boundary/STARS.java
+++ boundary/STARS.java
@@ -1,1 +1,13 @@
 package boundary;
+
+/*
+ * Main Application
+ *
+ * Done by AKSHITA RAVISANKAR, HOO KAH JUN, JEWELLE LIM FONG YEE, LEE YU JIE MELVIN, PUVVADA MEGHANA
+ */
+
+public class STARS {
+	public static void main(String argz[]) {
+
+	}
+}
