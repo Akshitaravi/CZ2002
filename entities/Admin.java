b37b9a3978fd4c0e2bd14e3f37decadc7f5d0764
pmeghana07 <71749547+pmeghana07@users.noreply.github.com>
2020-11-18 15:57:24 +0800

    Update Admin.java

--- entities/Admin.java
+++ entities/Admin.java
@@ -9,20 +9,23 @@ import java.util.Date;
  * Done by AKSHITA RAVISANKAR, HOO KAH JUN, JEWELLE LIM FONG YEE, LEE YU JIE MELVIN, PUVVADA MEGHANA
  */
 public class Admin extends User implements Serializable {
-	private String admin;
 
-	public Admin(String name, String username, String password, String nric, Date dob, int contact, boolean gender,
-			String nationality, String blockNo, String streetName, String cityName, String postalCode) {
-		super(name, username, password, nric, dob, contact, gender, nationality, blockNo, streetName, cityName,
-				postalCode);
-		this.admin = "admin";
+		// the ID of the Staff
+		private String staffID;
+	
+		//create an admin
+		public Admin() {
+			super();
+		}
+	
+		//Gets the staff  ID of this Admin.
+		public String getStaffID() {
+			return staffID;
+		}
+	
+		//change staff ID of this admin
+		public void setStaffNo(String staffNo) {
+			this.staffNo = staffNo;
+		}
+	
 	}
-
-	public String getAdmin() {
-		return admin;
-	}
-
-	public void setAdmin(String admin) {
-		this.admin = admin;
-	}
-}
